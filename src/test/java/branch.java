c2a36cdc366224649ae1277dc6548c2b567acc92
Ahmad T Farid <a.tariq.farid@gmail.com>
2023-10-05 19:31:14 -0400

    afterFix

--- src/test/java/branch.java
+++ src/test/java/branch.java
@@ -1,6 +1,7 @@
 public class branch {
     public static void main(String[] args) {
         System.out.println("for test");
+
+        System.out.println("after fixing the project");
     }
 }
-git 
